48d0d1749404c4471c1427072c65fd4c5dc0ac5f
satwinderbhullar <satwinderbhullar@10.51.206.187>
2019-09-05 11:19:15 -0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//output 
+	System.out.println("Example of github");
+	//input
+	System.out.println("whts ur name");
+	Scanner keyboard=new Scanner(System.in);
+	String personName=keyboard.nextLine();
+	System.out.println("hello" +"....." +personName);
+	
+
+	}
+
+}
